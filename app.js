8af3666047b58f6756196638d95dbf99fa02ad04
Alex <alex95.taylor@gmail.com>
2024-02-10 22:17:59 -0500

    Import inventory router

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const inventoryRouter = require('./routes/inventory');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
