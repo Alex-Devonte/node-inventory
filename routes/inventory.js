23a877a9fdaa96c6170ad2527c54b8c18cdb41d6
Alex <alex95.taylor@gmail.com>
2024-02-12 21:22:51 -0500

    Add category detail route

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,5 +11,6 @@ router.get('/items', itemController.item_list);
 router.get('/items/:id', itemController.item_detail);
 
 router.get('/categories', categoryController.category_list);
+router.get('/categories/:id', categoryController.category_detail);
 
 module.exports = router;
