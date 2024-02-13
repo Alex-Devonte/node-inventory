c86e8e65614fe1e6a7ee6834144fce538e79841e
Alex <alex95.taylor@gmail.com>
2024-02-12 21:18:53 -0500

    Add item field/ref to schema & update virtual url

--- models/category.js
+++ models/category.js
@@ -5,11 +5,12 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
     name: {type: String, required: true, minLength: 3},
     description: {type: String, maxLength: 50},
+    item: [{type: Schema.Types.ObjectId, ref: 'Item'}]
 });
 
 //Virtual for Category  URL
 CategorySchema.virtual('url').get(function() {
-    return `/inventory/${this._id}`;
+    return `/inventory/categories/${this._id}`;
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
