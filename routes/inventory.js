463ba970733116221cb9a020615da43446a14bfd
Alex <alex95.taylor@gmail.com>
2024-02-13 19:42:49 -0500

    Add multer middleware to router

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 const itemController = require('../controllers/itemController');
 const categoryController = require('../controllers/categoryController');
 
 router.get('/', itemController.index);
 
-router.get('/items', itemController.item_list);
+router.get('/items/create', itemController.create_item_get);
+router.post('/items/create', upload.single('item-img'), itemController.create_item_post);
 
+router.get('/items', itemController.item_list);
 router.get('/items/:id', itemController.item_detail);
 
 router.get('/categories', categoryController.category_list);
