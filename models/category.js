02c2b62e9cd49b46d8e6fbc78b44a6b1a752cf7e
Alex <alex95.taylor@gmail.com>
2024-02-18 13:59:14 -0500

    Update schema validation
    -Add maxLength validation to name property

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    name: {type: String, required: true, minLength: 3},
+    name: {type: String, required: true, minLength: 3, maxLength: 20},
     description: {type: String, maxLength: 50},
     item: [{type: Schema.Types.ObjectId, ref: 'Item'}]
 });
